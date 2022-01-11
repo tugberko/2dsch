7c280f837cb64ba37c09f7a3d2531b402e21765d
tugberko <67656059+tugberko@users.noreply.github.com>
2022-01-11 14:52:48 +0300

    Update 2dsch.py

--- 2dsch.py
+++ 2dsch.py
@@ -5,28 +5,31 @@ import numpy as np
 import scipy.linalg as la
 import math
 import datetime
-import scipy.signal
 import time
 
 start = time.time()
 
 
-
-# Simulation Parameters`
+# Simulation Parameters
 
 dt = 0.01  # Temporal seperation
 
 # Spatial grid points
-nx = 21
-ny = 21
+nx = 121
+ny = 121
 
-Lx = 12  # Spatial size, symmetric with respect to x=0
-Ly = 12  # Spatial size, symmetric with respect to x=0
+Lx = 11  # Spatial size, symmetric with respect to x=0
+Ly = 8.8  # Spatial size, symmetric with respect to x=0
 
-alpha=1
+kappa=4 # Anisotropy constant
 
-kx = 1
+
+x0 = 2
 ky = 1
+
+filename = 'xinitial' + str(x0) + 'ky' + str(ky) + '.dat'
+#filename = 'xinitial'+str(x0)+'+ky' + str(ky) +'.dat'
+number_of_oscillations = 20
 
 # Derived Simulation Parameters
 
@@ -57,9 +60,9 @@ def laplacian2D():
     return sp.kron(sp.eye(ny), Dxx) + sp.kron(Dyy,sp.eye(nx))
 
 # This function calculates the anisotropic potential energy at specified position.
-# alpha is defined in such a way that force constant omega_y^2 = alpha * omega_x^2   .
+# kappa is defined in such a way that force constant omega_y^2 = kappa * omega_x^2   .
 def Potential(posx, posy):
-    return 0.5*(posx**2) + 0.5*(alpha * (posy**2))
+    return 0.5*(posx**2) + 0.5*(kappa * (posy**2))
 
 # This function creates a discrete 2D potential energy matrix. It's compatible
 # with flattening with row priority.
@@ -75,16 +78,16 @@ def VMatrix():
 
 
 
-
-
 # This function creates a discrete 2D kinetic energy matrix.
 def TMatrix():
     return -0.5 * laplacian2D().toarray()
+
 
 # This function creates a discrete 2D Hamiltonian matrix.
 def HMatrix():
     return VMatrix() + TMatrix()
 
+# This function computes the sum of squares of each element of a 2D matrix.
 def SquareSum2D(some_psi):
     sum = 0
     for i in range(len(some_psi)):
@@ -122,10 +125,17 @@ def GroundStateExact2D():
         for j in range(nx):
             currentx = x[j]
             currenty = y[i]
-            psi = (alpha**(0.125))*(np.exp(-0.5*(currentx**2)))*((np.exp(-0.5*(alpha**0.5)*(currenty**2))))
-            result[i][j] = psi
 
-    return Normalize2D(result)
+            normalization_const = (kappa**(0.125))/(np.pi**0.5)
+
+            x_part = np.exp(-0.5 * currentx**2)
+
+            y_part = np.exp((kappa**0.5) * -0.5 * currenty**2)
+
+            result[i][j] = normalization_const * x_part * y_part
+
+    return result
+
 
 
 # Name says it all
@@ -134,6 +144,8 @@ def IdentityMatrix():
     for i in range(x.size*y.size):
         I[i][i] = 1
     return I
+
+
 
 def LHS():
     first_term = IdentityMatrix()
@@ -148,15 +160,19 @@ def RHS():
     return first_term - second_term
 
 
-
-
-LHS = LHS()
-RHS = RHS()
-InverseOfLHS = np.linalg.inv(LHS)
+def InitializeU():
+    ustart = time.time()
+    LHSmatrix = LHS()
+    RHSmatrix = RHS()
+    InverseOfLHS = np.linalg.inv(LHSmatrix)
+    ufinish = time.time()
+    elapsed = int(ufinish - ustart)
+    print( 'Calculation of U (' + str(nx) +' x '+ str(ny) + ') took ' + str(elapsed) + 's')
+    return np.matmul(InverseOfLHS, RHSmatrix)
 
 # U is the time evolution matrix. To keep things time efficient,
 # I initialize this matrix once and use it as necessary.
-U = np.matmul(InverseOfLHS, RHS)
+U = InitializeU()
 
 
 
@@ -184,7 +200,7 @@ def Overlap1D(psi1, psi2):
 
 
 def CoherentStateNumerical2D():
-    startingPoint = CoherentStateExact2D(0)
+    startingPoint = CoherentStateExact2D(x0, ky, 0)
     return startingPoint
 
 
@@ -192,96 +208,109 @@ def CoherentStateNumerical2D():
 #
 # https://files.slack.com/files-pri/T5MM8M0CR-F02LPDYN02X/download/2021-11-09-2d_coherent_state.pdf
 #
-# with x_0=2
-def CoherentStateExact2D(t):
-    xinitial = 3
+# with initial x and y displacements
+def CoherentStateExact2D(xinitial, ky, t):
     result = np.zeros((ny, nx), dtype=complex)
     for i in range(ny):
         for j in range(nx):
             currentx = x[j]
             currenty = y[i]
-            #A = (alpha**0.125)/(np.pi**0.5)
-            X = np.exp(  -0.5 * (currentx - xinitial * np.cos(t))**2    ) * np.exp(  -1j * xinitial * currentx * np.sin(t))
-            Y = np.exp(  -(0.5 * (alpha**0.5)) * (currenty - ky*np.sin(t*(alpha**0.5)))**2    ) * np.exp(  1j * ky * currenty * np.cos(t**(alpha**0.5)))
-            result[i][j] = X*Y
 
-    return Normalize2D(result)
+            normalization_const = (kappa**0.125)/(np.pi**0.5)
+
+            x_part = np.exp( -0.5 * ( currentx-xinitial*np.cos(t) )**2  ) * np.exp( -1j * currentx * xinitial  * np.sin(t) )
 
 
+            y_part = np.exp( (-0.5*np.sqrt(kappa)) * (currenty - (ky/np.sqrt(kappa))*np.sin(t * np.sqrt(kappa)) )**2  )       * np.exp( 1j * ky * currenty * np.cos(t*np.sqrt(kappa))   )
+
+            result[i][j] = normalization_const * x_part * y_part
+
+    return result
 
 
 
-number_of_oscillations = 5
+#1st checkpoint
+psi1 = CoherentStateExact2D(0, 0, 0)
+psi2 = CoherentStateExact2D(0, 0, 0)
+print(Overlap2D(psi1, psi2))
 
-terminateAt = number_of_oscillations * 2 * np.pi
-timesteps = math.ceil(terminateAt / dt)
 
-currentDate  = datetime.datetime.now()
 
-lx_over_kx = Lx/kx
-ly_over_ky = Ly/ky
 
-filename = 'default.dat'
 
-with open(filename, 'a') as f:
-    f.write('# Simulation started at ' + str(currentDate) + '\n')
-    f.write('#\n')
-    f.write('# Simulation details: \n')
-    f.write('# Lx = ' + str(Lx) + ' \n')
-    f.write('# kx = ' + str(kx) + ' \n')
-    f.write('# Lx / kx = ' + str(lx_over_kx) + ' \n')
 
-    f.write('# Ly = ' + str(Ly) + ' \n')
-    f.write('# ky = ' + str(ky) + ' \n')
-    f.write('# Ly / ky = ' + str(ly_over_ky) + ' \n')
-    f.write('# dt = ' + str(dt) + ' s \n')
-    f.write('# dx = ' + str(dx) + ' \n')
-    f.write('# dy = ' + str(dy) + ' \n')
-    f.write('# Spatial grid points : ' + str(nx) + ' x ' + str(ny) + ' \n')
-    f.write('#\n')
-    f.write('# time\terror\n')
+def Run():
+    terminateAt = number_of_oscillations * 2 * np.pi
+    timesteps = math.ceil(terminateAt / dt)
 
-psi_num = CoherentStateNumerical2D()
-
-for i in range(timesteps):
-    currentTime = i*dt
-
-    psi_exact = CoherentStateExact2D(currentTime)
-    error = np.abs(1 - Overlap2D(psi_num, psi_exact))
-
-    current_time_as_string = "{:.3f}".format(currentTime)
-    print('Current time: ' + current_time_as_string)
-    error_as_string = "{:.4e}".format(error)
+    currentDate  = datetime.datetime.now()
 
     with open(filename, 'a') as f:
-        f.write(current_time_as_string+'\t'+error_as_string+'\n')
+        f.write('# Simulation started at ' + str(currentDate) + '\n')
+        f.write('#\n')
+        f.write('# Simulation details: \n')
+
+        f.write('# x-initial = ' + str(x0) + ' \n')
+        f.write('# k_y = ' + str(ky) + ' \n')
+
+        f.write('# Lx = ' + str(Lx) + ' \n')
 
 
-    '''
-    plt.suptitle('Time: ' + current_time_as_string + '\nError: ' + error_as_string)
-
-    plt.subplot(1, 2, 1)
-    plt.contourf(x, y, np.abs(psi_num)**2, 256, cmap='RdGy')
-    plt.title('Numerical solution')
-    plt.xlim([-6, 6])
-    plt.ylim([-6, 6])
-    plt.grid()
-
-    plt.subplot(1, 2, 2)
-    plt.contourf(x, y, np.abs(psi_exact)**2, 256, cmap='RdGy')
-    plt.title('Exact solution')
-    plt.xlim([-6, 6])
-    plt.ylim([-6, 6])
-    plt.grid()
-
-    plt.tight_layout()
-    plt.savefig('frame'+str(i)+'.png')
-    plt.clf()
-    '''
-
-    psi_num = Evolve(psi_num)
+        f.write('# Ly = ' + str(Ly) + ' \n')
 
 
-end = time.time()
-difference = int(end - start)
-print( str(difference) + 's')
+        f.write('# dt = ' + str(dt) + ' s \n')
+        f.write('# dx = ' + str(dx) + ' \n')
+        f.write('# dy = ' + str(dy) + ' \n')
+        f.write('# Spatial grid points : ' + str(nx) + ' x ' + str(ny) + ' \n')
+        f.write('#\n')
+        f.write('# time\terror\n')
+
+    psi_num = CoherentStateNumerical2D()
+
+    for i in range(timesteps):
+        currentTime = i*dt
+
+        psi_exact = CoherentStateExact2D(x0, ky, currentTime)
+        error = np.abs(1 - Overlap2D(psi_num, psi_exact))
+
+        current_time_as_string = "{:.3f}".format(currentTime)
+        print('Current time: ' + current_time_as_string)
+        error_as_string = "{:.4e}".format(error)
+
+        with open(filename, 'a') as f:
+            f.write(current_time_as_string+'\t'+error_as_string+'\n')
+
+
+        '''
+        plt.suptitle('Time: ' + current_time_as_string + '\n ky = '+str(ky)+' , x_initial = '+str(x0)+' \nError: ' + error_as_string)
+
+        plt.subplot(1, 2, 1)
+        plt.contourf(x, y, np.abs(psi_num)**2, 256, cmap='RdGy')
+        plt.title('Numerical solution')
+        plt.xlim([-3, 3])
+        plt.ylim([-3, 3])
+        plt.grid()
+
+        plt.subplot(1, 2, 2)
+        plt.contourf(x, y, np.abs(psi_exact)**2, 256, cmap='RdGy')
+        plt.title('Exact solution')
+        plt.xlim([-3, 3])
+        plt.ylim([-3, 3])
+        plt.grid()
+
+        plt.tight_layout()
+        plt.savefig('visual'+str(i)+'.png')
+        plt.clf()
+
+        '''
+
+        psi_num = Evolve(psi_num)
+
+
+    end = time.time()
+    difference = int(end - start)
+    print( str(difference) + 's')
+    with open(filename, 'a') as f:
+        f.write('# Simulation finished at ' + str(currentDate) + '\n')
+        f.write('# Simulation took ' + str(difference) + ' seconds\n')
